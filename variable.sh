d8573031e9b0a0f53553125b020a1450bcb20226
Suysah Bobade <suyashbobade19095@gmail.com>
2025-04-15 17:57:47 +0000

    new one

---
+++ variable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#this file is known as variable because we can put the variable first as mentioned in below format then simply can call it with $ below as described
+
+name="Suyash Bobade"
+
+place="Navi Mumbai"
+
+company="Microsoft"
+
+
+echo "my name is $name and i have been living in $place since one year where i work for $company currently"
