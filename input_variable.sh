c380e9e3d7608b0ae25e9ae43c1036084e77f708
Suysah Bobade <suyashbobade19095@gmail.com>
2025-04-15 17:43:36 +0000

    new code

---
+++ input_variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#this file know as input variable where u can put variable it will provide promt to write and also display the same what u have entered
+
+echo "enter your name:"  #"enter your name" also supposed to work
+
+read username
+
+echo "you entered $username"
